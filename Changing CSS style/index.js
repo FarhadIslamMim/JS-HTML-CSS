1e60bb0c79ad6d64bd4c6c09ed8af8f4635264af
FarhadIslamMim <76618171+FarhadIslamMim@users.noreply.github.com>
2021-08-19 21:24:05 +0600

    Add files via upload

---
+++ Changing CSS style/index.js
@@ -0,0 +1,15 @@
+function addS(){
+   var myvar= document.querySelector("#pid");
+//    myvar.style.color="red";
+//    myvar.style.fontSize="4rem";
+//    myvar.style.fontweight="bold";
+//    myvar.style.fontStyle="italic";
+      myvar.classList.add("pera-style");
+
+}
+function removeS(){
+    var myvar= document.querySelector("#pid");
+ 
+       myvar.classList.remove("pera-style");
+ 
+ }
